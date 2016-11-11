f285a5ed2710fe131f82fe393a322bdd344e1815
Anu Nagra <anu.nagra@comprotechnologies.com>
2016-11-11 16:41:49 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 // config/db.js
     module.exports = {
         //url : 'mongodb://localhost::27017/annoucementBoard'
-        url: 'mongodb://<admin>:<admin>@ds149567.mlab.com:49567/annoucementboard'
+        url: 'mongodb://admin:admin@ds149567.mlab.com:49567/annoucementboard'
     }
